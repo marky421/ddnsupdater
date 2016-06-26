e6a32ef54215adf9b636efeb6bcdc39550abf106
Mark Spain <markyspain421@gmail.com>
2016-06-25 21:07:27 -0700

    remove API access dependency

--- ddns-update.py
+++ ddns-update.py
@@ -3,8 +3,7 @@
 import re, xml.etree.ElementTree as ET
 from sys import version_info
 
-PY3K = version_info >= (3, 0)
-if PY3K:
+if version_info >= (3, 0):
     import urllib.request as urllib
 else:
     import urllib2 as urllib
@@ -12,7 +11,7 @@ else:
 __version__ = "1.0"
 
 def getExternalIP():
-    regex = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+    regex = ('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' * 4)[:-2]
     ip = re.search(regex, httpGET('http://checkip.dyndns.org/plain')).group(0)
     return ip if len(ip) > 0 else ''
 
@@ -22,16 +21,58 @@ def httpGET(url):
     result.close
     return content
 
-def buildURL(host, domain, password, ip):
-    return 'https://dynamicdns.park-your-domain.com/update?host=' + host + '&domain=' + domain + '&password=' + password + '&ip=' + ip
-
+def buildUpdateURL(host, domain, password, ip):
+    args = {'host': host, 'domain': domain, 'password': password, 'ip': ip}
+    return buildCommandURL(args, 'https://dynamicdns.park-your-domain.com/update?')
+    
+def buildCommandURL(args, url = 'https://api.namecheap.com/xml.response?'):
+    return url + ''.join([(name + '=' + value + '&') for name, value in args.items()])[:-1]
 
 def getConfig():
     root = ET.parse('config.xml').getroot()
     config = {}
-    config['apiUser']  = root.find('APIUser').text
-    config['apiKey']   = root.find('APIKey').text
-    config['userName'] = root.find('UserName').text
+    config['ApiUser']  = root.find('ApiUser').text
+    config['ApiKey']   = root.find('ApiKey').text
+    config['UserName'] = root.find('UserName').text
+    config['ClientIp'] = getExternalIP()
     return config
 
-print getConfig()
+# uses the namecheap.com api
+def getDomains_api(config):
+    args = dict(config)
+    args['Command'] = 'namecheap.domains.getList'
+    url = buildCommandURL(args)
+    response = httpGET(buildCommandURL(args))
+    root = ET.fromstring(response)
+    ns = root.tag[:root.tag.find('}') + 1]
+    return [domain.get('Name') for domain in root.iter(ns + 'Domain')]
+
+# uses the namecheap.com api
+def getHostnames_api(config, domain):
+    args = dict(config)
+    args['Command'] = 'namecheap.domains.dns.getHosts'
+    args['SLD'], args['TLD'] = domain.split('.')
+    response = httpGET(buildCommandURL(args))
+    print response
+    root = ET.fromstring(response)
+    ns = root.tag[:root.tag.find('}') + 1]
+    return [host.get('Name') for host in root.iter(ns + 'Host')]
+
+def getDomains():
+    root = ET.parse('domains.xml').getroot()
+    return {domain.get('name'): domain.get('password') for domain in root.findall('Domain')}
+
+def getHostnames(domain):
+    root = ET.parse('domains.xml').getroot()
+    for d in root.findall('Domain'):
+        if d.get('name') == domain:
+            return [host.get('name') for host in d.iter('Host')]
+
+def updateDomains():
+    config = getConfig()
+    ip = getExternalIP()
+    for domain, password in getDomains().items():
+        updateURLs = [buildUpdateURL(host, domain, password, ip) for host in getHostnames(domain)]
+        print [httpGET(url) for url in updateURLs]
+
+updateDomains()
